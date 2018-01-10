27756b89884e99ea47582f3c2eb6ea0cb211a304
yinchangsheng <ychangsheng@gmail.com>
2018-01-10 19:31:29 +0800

    支持生产环境配置

--- src/config.js
+++ src/config.js
@@ -1,28 +1,42 @@
 
 const isProd = require('./env').isProd
+const isTest = require('./env').isTest
+const isDev = require('./env').isDev
 
 var config = {}
 
-if (isProd()) {
+if ( isProd() ) {
+  config = {
+    Domain: {
+      APIBaseUrl: 'https://wx.nizhuantech.com/api',
+      SiteBaseUrl: 'https://wx.nizhuantech.com'
+    }
+  }
+} else if ( isDev() ) {
   config = {
     Domain: {
       APIBaseUrl: "http://wxtest.qurenjia.com/api",
-      SiteBaseUrl: 'http://wxtest.qurenjia.com'
+      SiteBaseUrl: "http://wxtest.qurenjia.com"
+    }
+  }
+} else if ( isTest() ) {
+  config = {
+    Domain: {
+      APIBaseUrl: "http://wxdev.qurenjia.com/api",
+      SiteBaseUrl: "http://wxdev.qurenjia.com"
     }
   }
 } else {
   config = {
     Domain: {
-      // APIBaseUrl: "http://wxdev.qurenjia.com/api"
-      APIBaseUrl: "http://127.0.0.1:8081/api",
-      SiteBaseUrl: 'http://wxdev.qurenjia.com'
+      APIBaseUrl: "http://wxtest.qurenjia.com/api",
+      SiteBaseUrl: "http://wxtest.qurenjia.com"
     }
   }
 }
 
 module.exports = {
   Domain: {
-    AuthEntryPointUrl: config.Domain.AuthEntryPointUrl,
     APIBaseUrl: config.Domain.APIBaseUrl,
     SiteBaseUrl: config.Domain.SiteBaseUrl
   },
